beb11144203e6a0daf6a248f449bab18666df782
Akintade Ajibade <ajibade.akintade@gmail.com>
2021-02-01 20:53:52 +0000

    added all controller classes for book

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index(){
+       
+    }
+
+    public function store(Request $request){
+        
+    }
+
+    public function show($book){
+     
+
+    }
+
+    public function update(Request $request, $book){
+        
+    }
+
+    public function destroy($book){
+
+      
+    }
+}
